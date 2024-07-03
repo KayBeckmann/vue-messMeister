834080af96bef9c6c2fe33d9269b90b626abafb2
Kay <info@beckmann-md.de>
2024-07-03 19:36:29 +0200

    add all files

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="center">
+    <nav>
+      <RouterLink to="/">Home</RouterLink> |
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+.center {
+  text-align: center;
+}
+</style>
