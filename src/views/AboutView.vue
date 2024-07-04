c4c8f2c953fd0557c9d56df12815027d37a38dab
snuppedeluxe <snuppedeluxe@gmail.com>
2024-07-04 12:08:23 +0000

    Add Store to About

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="about">
     <h1>ABOUT</h1>
+    <button @click="counterStore.increaseCount">
+      {{ counterStore.count }}
+    </button>
   </div>
 </template>
+
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+
+const counterStore = useCounterStore()
+
+</script>
+
+<style scoped>
+.about{
+  font-size: 60px;
+  margin: 20px;
+}
+button{
+  font-size: 50px;
+  margin: 25px;
+  padding: 25px;
+}
+</style>
