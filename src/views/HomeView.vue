c581bcb6a8009f37d6070c58e754257c7dd8f8b3
snuppedeluxe <snuppedeluxe@gmail.com>
2024-07-04 10:41:15 +0000

    First steps

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,47 @@
 <template>
   <div class="home">
-    <h1>HOME</h1>
+    <div class="count">{{ count }}</div>
+    <div class="buttons">
+      <button @click="decreaseCount">-</button>
+      <button @click="increaseCount">+</button>
+    </div>
+    <div>
+      The counter is: {{ oddOrEven }}
+    </div>
   </div>
 </template>
+
+<script setup>
+import { computed, ref } from 'vue'
+
+const count = ref(2)
+
+const increaseCount = ()=>{
+  count.value++
+}
+
+const decreaseCount = ()=>{
+  if(count.value > 0){
+    count.value--
+  }
+}
+
+const oddOrEven = computed(()=>{
+  if(count.value % 2 === 0){
+    return 'even'
+  }else{
+    return 'odd'
+  }
+})
+</script>
+
+<style scoped>
+.count{
+  font-size: 60px;
+  margin: 20px;
+}
+.buttons{
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
